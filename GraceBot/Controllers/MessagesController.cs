5aba8de78117f7588bfe9d9d1bf48268553bffd1
stiege <alex.hodge@gmail.com>
2016-11-27 16:25:23 +1300

    Add dictionary

--- GraceBot/Controllers/MessagesController.cs
+++ GraceBot/Controllers/MessagesController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -17,17 +19,11 @@ namespace GraceBot
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity.Type == ActivityTypes.Message && await filter(activity))
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"Hi!, you sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await doResponse(activity);
             }
             else
             {
@@ -35,6 +31,77 @@ namespace GraceBot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private async Task<bool> filter(Activity activity)
+        {
+            foreach (var badWord in File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "\\BadWords\\en"))
+            {
+                if (activity.Text.ToLower().Contains(badWord.ToLower()))
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply("..."));
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static async Task doResponse(Activity activity)
+        {
+            string strEscaped = Uri.EscapeUriString(activity.Text);
+            string uri =
+                "https://api.projectoxford.ai/luis/v2.0/apps/" + Environment.GetEnvironmentVariable("LUIS_ID") +
+                "?subscription-key=" +
+                Environment.GetEnvironmentVariable("LUIS_KEY") + "&q=" +
+                strEscaped + "&verbose=true";
+            using (var client = new HttpClient())
+            {
+                var msg = await client.GetAsync(uri);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var response = JsonConvert.DeserializeObject<LuisResponse>(await msg.Content.ReadAsStringAsync());
+                    if (response.topScoringIntent.intent == "GetDefinition")
+                    {
+                        foreach (Entity responseEntity in response.entities)
+                        {
+                            if (responseEntity.type == "subject")
+                            {
+                                await findDefinitionFor(responseEntity.entity, activity);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static async Task findDefinitionFor(string subject, Activity activity)
+        {
+            using (var reader =
+                    new JsonTextReader(
+                        new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\Words\\dictionary.json"))
+            )
+            {
+                var definitions = new JsonSerializer().Deserialize<Dictionary<string, string>>(reader);
+                using (var connector = new ConnectorClient(new Uri(activity.ServiceUrl)))
+                {
+                    try
+                    {
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply(definitions[subject.ToUpper()]));
+                    }
+                    catch (Exception)
+                    {
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply(
+                            "I don't know about that yet..."));
+                    }
+                    
+                }
+            }
+        }
+
+        private static void log(string val)
+        {
+            System.Diagnostics.Debug.WriteLine(val);
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ GraceBot/LuisResponse.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraceBot
+{
+    public class LuisResponse
+    {
+        public string query { get; set; }
+        public Intent topScoringIntent { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+        public Dialog dialog { get; set; }
+    }
+
+    public class Action
+    {
+        public bool triggered { get; set; }
+        public string name { get; set; }
+        public Parameter[] parameters { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public bool required { get; set; }
+        public Value[] value { get; set; }
+    }
+
+    public class Value
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public Resolution resolution { get; set; }
+    }
+
+    public class Resolution
+    {
+    }
+
+    public class Dialog
+    {
+        public string contextId { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+        public Action[] actions { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public float score { get; set; }
+        public Resolution resolution { get; set; }
+    }
+
+}
