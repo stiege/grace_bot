5dc604de047d8c74f423d7519d96f9f5e3077bcd
Tao Liu <lioutau@gmail.com>
2017-02-03 16:48:28 +1300

    Creare a web app to manage question data

---
+++ GraceBotAdmin/Controllers/AdminController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GraceBot.Models;
+using GraceBotAdmin.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using ProcessStatus = GraceBot.Models.ProcessStatus;
+
+namespace GraceBotAdmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly GraceBotContext _db = new GraceBotContext();
+        // GET: Admin
+        public async Task<ActionResult> Index(string questionKeyWord,string processStatus)
+        {
+            var activityModels =await _db.Activities.
+                Include(a => a.From).
+                Include(a => a.From).
+                Include(a => a.Recipient).
+                Include(a => a.Conversation).
+                ToListAsync();
+
+            var activitiesFiltered = activityModels;
+
+            if (!string.IsNullOrEmpty(questionKeyWord))
+            {
+                activitiesFiltered = activityModels.Where(o => o.Text.Contains(questionKeyWord)).ToList();
+            }
+            if (!string.IsNullOrEmpty(processStatus))
+            {
+                try
+                {
+                    var processStatusInt =(ProcessStatus) Convert.ToInt32(processStatus);
+                    activitiesFiltered = activityModels.Where(o => o.ProcessStatus.Equals(processStatusInt)).ToList();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            var activityViewModels = new List<ActivityViewModels.ActivityAnswerViewModel>();
+
+            foreach (var activity in activitiesFiltered)
+            {
+                if (!string.IsNullOrEmpty(activity.ReplyToId))
+                {
+                    var questionActivity = activityModels.FirstOrDefault(o => o.ActivityId != null && o.ActivityId.Equals(activity.ReplyToId));
+                    activityViewModels.Add(new ActivityViewModels.ActivityAnswerViewModel()
+                    {
+                        Question = questionActivity.Text,
+                        Channel = questionActivity.ChannelId,
+                        UserName = questionActivity.From.Name,
+                        Date = questionActivity.Timestamp,
+                        Answer = activity?.Text,
+                        AnsweredBy = activity.From.Name,
+                        DateOfQuestionAnswered = activity?.Timestamp,
+                    });
+                }
+                else if (activity.ProcessStatus.Equals(ProcessStatus.Unprocessed))
+                {
+                    activityViewModels.Add(new ActivityViewModels.ActivityAnswerViewModel()
+                    {
+                        Question = activity.Text,
+                        Channel = activity.ChannelId,
+                        UserName = activity.From.Name,
+                        Date = activity.Timestamp,
+                        Answer = String.Empty,
+                        AnsweredBy = String.Empty,
+                        DateOfQuestionAnswered = null,
+                    });
+                }
+            }
+
+
+
+            return View(activityViewModels.OrderByDescending(o => o.Date).ToList());
+        }
+    }
+}
---
+++ GraceBotAdmin/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GraceBotAdmin.Startup))]
+namespace GraceBotAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            // Custom datadirectory
+            var path = AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
+            path=path.Replace("GraceBotAdmin", "GraceBot");
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+    }
+}
